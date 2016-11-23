cf8f8b98f103c9c6a7f7ba2977c18d596164c939
Raasil <14rk45@linux1.caslab.queensu.ca>
2016-11-23 18:51:30 -0500

    just practice code chill fam

--- capitalizer.c
+++ capitalizer.c
@@ -1,1 +1,22 @@
-// this is lit 
+// capitalizer program
+
+#include <stdio.h>
+
+int main()
+{
+	int distance = 100;
+	float power = 2.345f;
+	double super_power = 56789.4532;
+	char initial = 'A';
+	char first_name[] = "Zed";
+	char last_name[] = "Shaw";
+
+	printf("you are %d miles away.\n", distance);
+	printf("You have %f levels of power.\n", power);
+	printf("my whole name is %s %c. %s. \n",  first_name, initial, last_name);
+
+	return 0;
+
+
+}	
+

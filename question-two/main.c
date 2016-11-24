44f9d92fb785e52448b0ab3bf95d53094ad0a59b
StefanSokic <stefan.sokic33@gmail.com>
2016-11-24 17:45:37 -0500

    declare functions

---
+++ question-two/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+	
+	// first call the safestrcat
+	char a[5] = "west";
+	char b[4] = "tri";
+	safestrcat(a, b);
+	
+	// next we call the substring function
+	char source[6] = "yeezy";
+	int from = 0;
+	int n = 3;
+	char target[10] = "yeezywest";
+	substring(source, from, n, target);
+	return 0;
+}
---
+++ question-two/mystring.c
@@ -0,0 +1,14 @@
+#include<stdio>
+
+char* safestrcat(char *a, char *b) {
+	char newStr[] = "";
+
+
+	return newStr;
+	}
+
+int substring(char *source, int from, int n, char *target) {
+
+
+	return 0;
+	}

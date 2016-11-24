fa670d851e9ff4adb90f9f9168dbce085228aba3
Raasil <14rk45@linux1.caslab.queensu.ca>
2016-11-23 19:23:48 -0500

    more pracky

--- capitalizer.c
+++ capitalizer.c
@@ -1,22 +1,27 @@
-// capitalizer program
-
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	int distance = 100;
-	float power = 2.345f;
-	double super_power = 56789.4532;
-	char initial = 'A';
-	char first_name[] = "Zed";
-	char last_name[] = "Shaw";
+	int bugs = 100;
+	double bug_rate = 1.2;
 
-	printf("you are %d miles away.\n", distance);
-	printf("You have %f levels of power.\n", power);
-	printf("my whole name is %s %c. %s. \n",  first_name, initial, last_name);
+	printf("you have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
 
-	return 0;
+	long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+	printf("The entire universe has %ld bugs.\n", universe_of_defects);
 
+	double expected_bugs = bugs * bug_rate;
+	printf("You are expected to have %f bugs.\n", expected_bugs);
 
-}	
+	double part_of_universe = expected_bugs / universe_of_defects;
+	printf("That is only a %e portion of the universe.\n", part_of_universe);
+
+	
+	char nul_byte = '\0';
+	int care_percentage = bugs * nul_byte;
+	printf("Which means you should care %d%%.\n", care_percentage);
+
+	return 0
+}
+
 
